17d42c4ab0354ecb8f8f7d28a92ae6a6a446282a
Andrew Hall <u5825803@anu.edu.au>
2016-01-10 21:45:43 +1100

    Implement Execute.hs using new MuApi.hs

--- src/LibMu/Execute.hs
+++ src/LibMu/Execute.hs
@@ -12,16 +12,15 @@ import Foreign.C.String
 runBundle :: String -> IO ()
 runBundle file = do
   mvm <- mu_refimpl2_new
-  ctx <- call0 mkNewContext new_context mvm
+  ctx <- newContext mvm
   
-  ctxVal <- peek ctx
   withCString file $ \fileStr -> do
-    mkLoadBundle (load_bundle ctxVal) ctx fileStr (fromIntegral $ length file)
+    loadBundle ctx fileStr (fromIntegral $ length file)
   withCString "@main" $ \main_name -> do
-    main_id <- call1 mkCtxIdOf ctx_id_of ctx main_name
-    func <- mkHandleFromFunc (handle_from_func ctxVal) ctx main_id
-    stack <- mkNewStack (new_stack ctxVal) ctx func
-    _ <- mkNewThread (new_thread ctxVal) ctx stack 1 nullPtr 0 nullPtr
+    main_id <- ctxIdOf ctx main_name
+    func <- handleFromFunc ctx (fromIntegral main_id)
+    stack <- newStack ctx func
+    _ <- newThread ctx stack 1 nullPtr 0 nullPtr
   
-    call0 mkExecute execute mvm
-  mkCloseContext (close_context ctxVal) ctx
+    execute mvm
+  closeContext ctx
