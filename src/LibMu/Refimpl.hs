1c1ae783d2865ea7de9677a84c3a10aaa3462a0a
nathyong <nathyong@gmail.com>
2016-04-08 17:45:11 +1000

    Remove modules that actually call Mu functions

--- src/LibMu/Refimpl.hs
+++
@@ -1,8 +0,0 @@
-{-#LANGUAGE NoImplicitPrelude, ForeignFunctionInterface#-}
-module LibMu.Refimpl where
-
-import Prelude (IO)
-import LibMu.MuApi (MuVM, Ptr, Int64_t, CLong(..))
-
-foreign import ccall "refimpl2-start.h mu_refimpl2_new" mu_refimpl2_new :: IO (Ptr MuVM)
-foreign import ccall "refimpl2-start.h mu_refimpl2_new_ex" mu_refimpl2_new_ex :: Int64_t -> Int64_t -> Int64_t -> IO (Ptr MuVM)
